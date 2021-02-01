8c80d6e9c760ffb84273eb869f6bf4ba7fb4c30a
pb <przemyslaw.bojczuk@atos.net>
2021-02-01 21:00:31 +0100

    first commit

---
+++ updateConfigs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 02/01/2021; przemys1aw; created
+
+ACONFFILES=(.editorconfig
+            .nanorc
+           )
+
+for CONFFILE in ${ACONFFILES[*]}; do
+
+  if [[ -L "${HOME}/${CONFFILE}" ]]; then
+    unlink ${HOME}/${CONFFILE} && echo "Unlinked ${CONFFILE}"
+  elif [[ -f "${HOME}/${CONFFILE}" ]]; then
+    mv ${HOME}/${CONFFILE} ${HOME}/${CONFFILE}_$(date "+%Y%m%d%H%M%S") && "Moved original ${CONFFILE}"
+  else
+    "Echo ${CONFFILE} not found"
+  fi
+
+  ln -s ${PWD}/${CONFFILE} ${HOME}/${CONFFILE} && echo "Link created to ${CONFFILE}"
+
+done
+
+echo -e "\nParanoid check:"
+for CONFFILE in ${ACONFFILES[*]}; do
+
+  ls -l ${HOME}/${CONFFILE}
+
+done
